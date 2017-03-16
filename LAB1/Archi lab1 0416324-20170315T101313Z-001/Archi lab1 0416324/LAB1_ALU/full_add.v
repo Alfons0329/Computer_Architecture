f5acd3ff3e324fe9a796cb617f845e33bb9b2f41
Alfons Hwu <alfons.cs04@g2.nctu.edu.tw>
2017-03-16 18:38:27 +0800

    Update full_add.v

--- LAB1/Archi lab1 0416324-20170315T101313Z-001/Archi lab1 0416324/LAB1_ALU/full_add.v
+++ LAB1/Archi lab1 0416324-20170315T101313Z-001/Archi lab1 0416324/LAB1_ALU/full_add.v
@@ -1,11 +1,17 @@
 module full_add(x,y,cin,s,cout);
-	input x,y,cin;
-	output s,cout;
-	wire s1,c1,c2,c3;
-	xor(s1,x,y);
-	xor(s,cin,s1);
-	and(c1,x,y);
-	and(c2,y,cin);
-	and(c3,x,cin);
-	or(cout,c1,c2,c3);
+
+	input wire x,y,cin;
+	output wire s,cout;
+         // wires (from ands to or)
+   wire w1, w2, w3;
+         
+         // carry-out circuitry
+   and( w1, x, y);
+   and( w2, x, cin );
+   and( w3, y, cin );
+   assign cout= |{w1,w2,w3};
+         
+         // sum
+   xor( s, x, y, cin );
+	//assign  {cout,s}=x+y+cin;
 endmodule
